7007df7d6755c2c697c866cdcc57af1a7b8bcbf0
Ramu N <ramun@goggery.com>
2020-05-01 23:17:31 +0530

    added popup

--- DigitalOrderTerminal/digiorder_terminal/Controllers/HomeController.cs
+++ DigitalOrderTerminal/digiorder_terminal/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using digiorder_terminal.Models;
 
 namespace digiorder_terminal.Controllers
 {
@@ -20,6 +21,20 @@ namespace digiorder_terminal.Controllers
         public ActionResult Menu(String GUID)
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult getAddOnsById()
+        {
+            List<AddOnModel> empList = new List<AddOnModel>()
+             {
+                 new AddOnModel{ID = 1, Title = "ONION", Price="10.00", Image = "onion.png"},
+                 new AddOnModel{ID = 2, Title = "GHEE", Price="5.00", Image = "ghee.png"},
+                 new AddOnModel{ID = 3, Title = "KARA", Price="10.00", Image = "kara.png"},
+                 new AddOnModel{ID = 4, Title = "UPMA", Price="15.00", Image = "upma.png"}
+             };
+
+            return PartialView("Partial/_AddOns", empList);
         }
 
     }
